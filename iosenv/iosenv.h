3110973df2bc19c1d1109972aff2ad0d94afafc9
stanislaw <s.pankevich@gmail.com>
2013-09-30 21:45:43 +0300

    Cut off non-iosenv stuff

--- iosenv/iosenv.h
+++ iosenv/iosenv.h
@@ -82,140 +82,6 @@ static inline BOOL isiPhone5() {
 }
 
 #pragma mark
-#pragma mark File system
-
-static inline NSURL *applicationDocumentsDirectoryURL() {
-    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-}
-
-static inline NSString *applicationDocumentsDirectoryPath() {
-    return applicationDocumentsDirectoryURL().path;
-}
-
-static inline unsigned long long int folderSize(NSString *folderPath) {
-    NSArray *filesArray = [NSFileManager.defaultManager subpathsOfDirectoryAtPath:folderPath error:nil];
-
-    NSString *fileName;
-    unsigned long long int fileSize = 0;
-
-    for (fileName in filesArray) {
-        NSDictionary *fileDictionary = [NSFileManager.defaultManager attributesOfItemAtPath:[folderPath stringByAppendingPathComponent:fileName] error:nil];
-        fileSize += [fileDictionary fileSize];
-    }
-
-    return fileSize;
-}
-
-#pragma mark
-#pragma mark WWW: User agent
-
-static inline NSString *userAgentString() {
-    UIWebView* webView = [[UIWebView alloc] initWithFrame:CGRectZero];
-    NSString* secretAgent = [webView stringByEvaluatingJavaScriptFromString:@"navigator.userAgent"];
-
-    return secretAgent;
-}
-
-#pragma mark
-#pragma mark Cleaning
-
-static inline void cleanTemporaryDirectory() {
-    NSString *directory = NSTemporaryDirectory();
-
-    NSLog(@"Cleaning temporary directory: %@, %llu", directory, folderSize(directory));
-
-    NSError *error = nil;
-
-    for (NSString *file in [NSFileManager.defaultManager contentsOfDirectoryAtPath:directory error:&error]) {
-        NSLog(@"Removing: %@", file);
-
-        [NSFileManager.defaultManager removeItemAtPath:[NSString stringWithFormat:@"%@%@", directory, file] error:&error];
-
-        if (error) {
-            NSLog(@"Error deleting file: %@", file);
-        }
-    }
-
-    NSLog(@"Cleaning completed...");
-}
-
-static inline void cleanCookies() {
-    NSHTTPCookie *cookie;
-    NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
-    for (cookie in [storage cookies])
-    {
-        [storage deleteCookie:cookie];
-    }
-}
-
-#pragma mark
-#pragma mark Displaying system information
-
-static inline void NSLogCookies() {
-    NSHTTPCookie *cookie;
-    NSLog(@"==== Cookies ====");
-    NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
-    for (cookie in [storage cookies])
-    {
-        NSLog(@"\tCookie: %@", cookie);
-    }
-}
-static inline void NSLogUserDefaults() {
-    NSLog(@"==== NSUserDefaults ====");
-    NSLog(@"\t%@", [[NSUserDefaults standardUserDefaults] dictionaryRepresentation]);
-}
-
-static inline void NSLogFonts() {
-    NSLog(@"==== Available fonts ====");
-    NSArray *familyNames = [UIFont familyNames];
-
-    for( NSString *familyName in familyNames ){
-        NSLog(@"\tFamily: %s \n", [familyName UTF8String] );
-
-        NSArray *fontNames = [UIFont fontNamesForFamilyName:familyName];
-        for( NSString *fontName in fontNames ){
-            NSLog(@"\t\tFont: %s\n", [fontName UTF8String] );
-        }
-    }
-}
-
-static inline void NSLogSystemVersion() {
-    printf("\n");
-
-    NSLog(@"==== System version ====");
-    NSLog(@"%@", UIDevice.currentDevice.systemVersion);
-
-    printf("\n");
-}
-
-static inline void NSLogScreenDimensions() {
-    printf("\n");
-    
-    NSLog(@"==== Screen dimensions ====");
-
-    CGRect bounds = UIScreen.mainScreen.bounds;
-    NSLog(@"Screen bounds: x:%f, y:%f, width:%f, height:%f", bounds.origin.x, bounds.origin.y, bounds.size.width, bounds.size.height);
-
-    CGRect frame = UIScreen.mainScreen.applicationFrame;
-    NSLog(@"Screen application frame: x:%f, y:%f, width:%f, height:%f", frame.origin.x, frame.origin.y, frame.size.width, frame.size.height);
-
-    NSLog(@"Main screen scale: %f", UIScreen.mainScreen.scale);
-
-    NSLog(@"Real screen size: %@", UIScreen.mainScreen.currentMode);
-
-    printf("\n");
-}
-
-static inline void NSLogTemporaryDirectoryPath() {
-    printf("\n");
-
-    NSLog(@"==== Temporary directory ====");
-    NSLog(@"%@", NSTemporaryDirectory());
-    
-    printf("\n");
-}
-
-#pragma mark 
 #pragma mark Deprecations
 
 __attribute__((deprecated("Use isSystemVersionEqualTo")))
@@ -253,14 +119,12 @@ static inline BOOL isIPhone5() {
     return isiPhone5();
 }
 
-__attribute__((deprecated("interfaceOrientation() has been moved into FoundationExtensions project")))
+__attribute__((deprecated("interfaceOrientation() has been moved into UIKitExtensions project")))
 static inline UIInterfaceOrientation interfaceOrientation() {
     return UIApplication.sharedApplication.statusBarOrientation;
 }
 
-__attribute__((deprecated("isPortraitOrientation() has been moved into FoundationExtensions project")))
+__attribute__((deprecated("isPortraitOrientation() has been moved into UIKitExtensions project")))
 static inline BOOL isPortraitOrientation() {
     return UIInterfaceOrientationIsPortrait(interfaceOrientation());
 }
-
-
