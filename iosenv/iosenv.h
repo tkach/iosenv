d70edbca22daeb952a2078178f21c0cd939a07e4
atkachenko <atkachenko@gmail.com>
2014-02-10 15:56:30 +0200

    Added isJailbroken() check

--- iosenv/iosenv.h
+++ iosenv/iosenv.h
@@ -28,6 +28,43 @@ static inline BOOL isiOS7OrHigher() {
     return isSystemVersionHigherThanOrEqual(@"7.0");
 }
 
+static inline BOOL isJailbroken() {
+#if !(TARGET_IPHONE_SIMULATOR)
+    if ([[NSFileManager defaultManager] fileExistsAtPath:@"/Applications/Cydia.app"]) {
+        return YES;
+    }
+    else if ([[NSFileManager defaultManager] fileExistsAtPath:@"/Library/MobileSubstrate/MobileSubstrate.dylib"]) {
+        return YES;
+    }
+    else if ([[NSFileManager defaultManager] fileExistsAtPath:@"/bin/bash"]) {
+        return YES;
+    }
+    else if ([[NSFileManager defaultManager] fileExistsAtPath:@"/usr/sbin/sshd"]) {
+        return YES;
+    }
+    else if ([[NSFileManager defaultManager] fileExistsAtPath:@"/etc/apt"]) {
+        return YES;
+    }
+    
+    NSError *error;
+    NSString *stringToBeWritten = @"This is a test.";
+    [stringToBeWritten writeToFile:@"/private/jailbreak.txt" atomically:YES
+                          encoding:NSUTF8StringEncoding error:&error];
+    if (error==nil){
+        return YES;
+    } else {
+        [[NSFileManager defaultManager] removeItemAtPath:@"/private/jailbreak.txt" error:nil];
+    }
+    
+    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"cydia://package/com.example.package"]]){
+        return YES;
+    }
+#endif
+    
+    //All checks have failed. Most probably, the device is not jailbroken
+    return NO;
+}
+
 #pragma mark
 #pragma mark Production or development?
 
